18efa65a7e0ea636f369fbfc4044e816ddb3e243
alhanger <hanger129@gmail.com>
2015-11-19 16:08:21 -0500

    Create classes, repositories, controller and passwordhash

---
+++ src/main/java/com/theironyard/entities/Gift.java
@@ -0,0 +1,12 @@
+package com.theironyard.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by alhanger on 11/19/15.
+ */
+@Entity
+@Table(name = "gifts")
+public class Gift {
+}
