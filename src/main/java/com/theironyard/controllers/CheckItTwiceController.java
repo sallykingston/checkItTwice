51c6ee9c222a4a6f9cfeccf7a370e786326f69ec
alhanger <hanger129@gmail.com>
2015-11-19 18:04:30 -0500

    Create findOneByUsername method in UsersRepo and add-recipient route

--- src/main/java/com/theironyard/controllers/CheckItTwiceController.java
+++ src/main/java/com/theironyard/controllers/CheckItTwiceController.java
@@ -1,9 +1,15 @@
 package com.theironyard.controllers;
 
+import com.theironyard.entities.Recipient;
+import com.theironyard.entities.User;
 import com.theironyard.services.GiftRepository;
 import com.theironyard.services.RecipientRepository;
 import com.theironyard.services.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.math.BigDecimal;
 
 /**
  * Created by alhanger on 11/19/15.
@@ -18,4 +24,17 @@ public class CheckItTwiceController {
 
     @Autowired
     GiftRepository gifts;
+
+    @RequestMapping("/add-recipient")
+    public void addRecipient(HttpSession session, String name, BigDecimal budget) {
+        String username = (String) session.getAttribute("username");
+        User user = users.findOneByUsername(username);
+
+        Recipient recipient = new Recipient();
+        recipient.name = name;
+        recipient.budget = budget;
+        recipients.save(recipient);
+
+        user.recipientList.add(recipient);
+    }
 }
--- src/main/java/com/theironyard/services/UserRepository.java
+++ src/main/java/com/theironyard/services/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by alhanger on 11/19/15.
  */
 public interface UserRepository extends CrudRepository<User, Integer> {
+    User findOneByUsername(String username);
 }
