3e4d4755dd1f580393e8dc6e42fdce10b801f00c
alhanger <hanger129@gmail.com>
2015-11-19 16:40:04 -0500

    Created fields in each class and Autowired repos in controller

--- src/main/java/com/theironyard/entities/Gift.java
+++ src/main/java/com/theironyard/entities/Gift.java
@@ -1,7 +1,7 @@
 package com.theironyard.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.math.BigDecimal;
 
 /**
  * Created by alhanger on 11/19/15.
@@ -9,4 +9,16 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "gifts")
 public class Gift {
+    @Id
+    @GeneratedValue
+    public int id;
+
+    @Column(nullable = false)
+    public String name;
+
+    @Column
+    public BigDecimal cost;
+
+    @ManyToOne
+    public Recipient recipient;
 }
