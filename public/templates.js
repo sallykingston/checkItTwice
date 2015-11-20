20f8d187ac9f0b5b8324179ed3b2df04fd778cb6
Jacob Sharp <sidewayseightgmod@gmail.com>
2015-11-20 13:11:03 -0500

    merging templates

--- public/templates.js
+++ public/templates.js
@@ -6,11 +6,9 @@ module.exports = {
     "<h3><%= giftCost %></h3>",
     "<h4><%= giftId %></h4>",
   ].join(''),
-
   header: [
       "<h1>Check It Twice</h1>"
   ].join(''),
-
   footer: [
       "<h1>Don't be Nawwwty</h1>"
   ].join(''),
