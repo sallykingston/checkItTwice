9afc501b9fd9206828d2ed4a08db447b69669d35
Sally Kingston <sally.a.kingston@gmail.com>
2015-11-19 19:29:39 -0500

    recipient model, collection, and views added

---
+++ public/recipientCollectionView.js
@@ -0,0 +1,24 @@
+var Backbone = require('backbone');
+var _ = require('underscore');
+var $ = require('jquery');
+Backbone.$ = $;
+var RecipientView = require('./recipientCollectionView');
+var RecipientModel = require('./recipientModel')
+
+module.exports = Backbone.View.extend({
+  el: '.recipients',
+  initialize: function () {
+    this.addAll();
+    this.listenTo(this.collection, 'change', this.addAll);
+    this.listenTo(this.collection, 'sort', this.addAll);
+  },
+  addOne: function (recipientModel) {
+    var recipientView = new RecipientView({model: recipientModel});
+    this.$el.append(recipientView.render().el);
+
+  },
+  addAll: function () {
+    this.$el.html("");
+    _.each(this.collection.models, this.addOne, this);
+  },
+})
---
+++ public/recipientModel.js
@@ -0,0 +1,17 @@
+// Recipient Model
+
+var Backbone = require('backbone');
+
+module.exports = Backbone.Model.extend({
+  urlRoot: 'http://tiny-tiny.herokuapp.com/collections/checkItTwiceRecipients',
+  idAttribute: '_id',
+  defaults: {
+    name: "Buddy the Elf"
+    budget: 0,
+    giftList: null,
+
+  },
+  initialize: function () {
+
+  }
+});
