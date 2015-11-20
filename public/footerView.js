7db7455128e080cc5716d258fb0b5e2ca464c111
Jared Bilton <cantops@jareds-mbp.attlocal.net>
2015-11-19 20:56:46 -0500

    Header.js and Footer.js with template insert

---
+++ public/footerView.js
@@ -0,0 +1,16 @@
+var Backbone = require('backbone');
+var _ = require('underscore');
+var $ = require('jquery');
+Backbone.$ = $;
+var tmpl = require('./templates');
+
+  module.exports = Backbone.View.extend({
+    initialize: function () {};
+    template: _.template(tmpl.footer),
+    render: function () {
+      var markup = this.template({
+        this.$el.html(markup);
+        return this;
+      })
+    }
+  });
---
+++ public/templates.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+  header: [
+      "<h1>Check It Twice</h1>"
+    ].join(''),
+  footer: [
+      "<h1>Don't be Nawwwty</h1>"
+    ].join('')
+}
